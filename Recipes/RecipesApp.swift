8c0578265926c30a37dfadfe12ac9a1e8bc5f659
Kevinwtierney <kevinwtierney@gmail.com>
2022-02-25 16:19:54 -0800

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Kevin Tierney on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
