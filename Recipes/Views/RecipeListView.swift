02356f18d6b42b3985fa6e4424e46723ce335288
Kevinwtierney <kevinwtierney@gmail.com>
2022-03-25 13:35:32 -0700

    change font to Avenir on all views

--- Recipes/Views/RecipeListView.swift
+++ Recipes/Views/RecipeListView.swift
@@ -19,7 +19,7 @@ struct RecipeListView: View {
                 Text("All Recipes")
                     .bold()
                     .padding(.top,40)
-                    .font(.largeTitle)
+                    .font(Font.custom("Avenir Heavy", size: 24))
                 
                 ScrollView{
                     
@@ -40,7 +40,7 @@ struct RecipeListView: View {
                                     VStack(alignment: .leading){
                                         Text(r.name)
                                             .foregroundColor(.black)
-                                            .bold()
+                                            .font(Font.custom("Avenir Heavy", size: 16))
                                         RecipeHighlightsView(highlights: r.highlights)
                                             .foregroundColor(.black)
                                     }
